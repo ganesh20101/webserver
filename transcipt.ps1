bcb3dfd8cea6663331b4aa3ac7a84b617b9b6d47
ganesh20101 <115871700+ganesh20101@users.noreply.github.com>
2023-07-26 18:48:18 +0530

    Add files via upload

---
+++ transcipt.ps1
@@ -0,0 +1,10 @@
+﻿Start-Transcript -Append C:\Users\satvat\Desktop\PSScriptLog.txt
+
+Get-Process| where-object {$_.WorkingSet -GT 500000*1024}|select processname,@{l="Used RAM(MB)"; e={$_.workingset / 1mb}} |sort "Used RAM(MB)" –Descending
+
+
+
+Stop-Transcript
+
+
+
