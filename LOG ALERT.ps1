d6d8d2f3a15ea59fafdef2b6fb6276efd96b4d42
ganesh20101 <115871700+ganesh20101@users.noreply.github.com>
2023-07-26 18:06:22 +0530

    Add files via upload

---
+++ LOG ALERT.ps1
@@ -0,0 +1,2 @@
+﻿
+powershell -WindowStyle hidden -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('SOFTWARES & SERVICES,IIS,FIREWALL SETIINGS SUCCESSFULLY LOADED ONCE CONFIRM WITH VERIFY TEAM','LOG ALERT')}"
