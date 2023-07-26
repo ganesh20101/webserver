9afe2fc3e8bd5b5a0e500bb2e33553c61a3e3e4a
ganesh20101 <115871700+ganesh20101@users.noreply.github.com>
2023-07-26 18:23:59 +0530

    Add files via upload

---
+++ LOGMSG.ps1
@@ -0,0 +1,10 @@
+﻿Function Log-Message([String]$Message)
+{
+    Add-Content -Path "C:\Users\satvat\Desktop\Log.txt" $Message
+}
+ 
+Log-Message "Beginning exeuction of the script:"
+Log-Message "Exeucting of the script..."
+Log-Message "Completed exeuction of the script!"
+
+
